1c2ddf63b47f84b7336092536bb9b7dceb8f936f
Thilina kavishan <113406436+thilina32@users.noreply.github.com>
2024-05-20 16:33:30 +0530

    Create _autoReply.js

---
+++ plugins/_autoReply.js
@@ -0,0 +1,17 @@
+
+import fetch from 'node-fetch';
+import { promises as fs } from 'fs';
+export async function all(m) {
+    // when someone sends a group link to the bot's dm
+    if (
+      !m.isBaileys &&
+      !m.isGroup
+    ) {
+      
+      
+      
+  //m.zreact('❤')+
+      
+  return !0
+}
+}
